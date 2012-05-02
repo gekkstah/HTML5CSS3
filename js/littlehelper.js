3c50690a2a3bf4dce3f2548bc5be3caa2a898d04
Nils Lauk <nils.lauk@xing.com>
2012-05-02 16:45:01 +0200

    threw away all the old stuff, now with s5

--- js/littlehelper.js
+++
@@ -1,8 +0,0 @@
-// Hole Breiten von IMG in FIGURE und apply sie auf FIGURE
-// Muss sein, weil sonst FIGCAPTION über FIGURE hinaus läuft
-$(document).ready(function() {
-	$("body").find("figure").each(function() {
-		var picWidth = $(this).find("img").width();
-		$(this).css("width", picWidth);		
-	});
-});
